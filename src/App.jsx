74cb22300921dbdd9629b8360a35a19f5730e727
Segun Ajibola <ajbl.segun@gmail.com>
2025-02-25 07:25:46 +0100

    hero-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import "./App.css";
-import { Button } from "@/components/ui/button"
-// C:\Users\ajbls\Desktop\Programming\ptg\pagestrap\src\components\ui\button.jsx
-// import { Button } from "./../src/components/ui/button.jsx"
+import { Button } from "@/components/ui/button";
+import { Navbar, Hero } from "./section";
 
 function App() {
-
   return (
     <>
-      <div>
-        <Button>Click me</Button>
-      </div>
-      <p className="text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Hero />
+      
     </>
   );
 }
